e42497eae8da42d6f34790cf5b24c0acdcc18aed
indra.mulyanto <indra.mulyanto@greatdayhr.com>
2019-03-05 12:51:16 +0700

    hbs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VideostreamController } from './controllers/videostream.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [HttpModule],
+  controllers: [AppController, VideostreamController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controllers/videostream.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, HttpService, Req, Res, Render } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Controller('videostream')
+export class VideostreamController {
+  constructor(private http: HttpService) {}
+
+  @Get()
+  @Render('index')
+  async getVideoStream(@Res() res) {
+    return { message: 'video stream' };
+  }
+
+  @Get('video')
+  async getVideo(@Res() res, @Req() req) {
+    const link = __dirname + '../../assets/sample.mp4';
+    const stat = fs.statSync(link);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunksize = (end - start) + 1;
+      const file = fs.createReadStream(link, {start, end});
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(link).pipe(res);
+    }
+  }
+}
